fd3623f9537b83bea2d7709b903570f5046366c3
Dan O'Shea <dan@djoshea.com>
2009-11-16 12:13:11 -0800

    clicking point opens synaptogram

--- synplot.m
+++ synplot.m
@@ -4,12 +4,13 @@ features = {'IB_VGlut1', 'IB_PSD95'};
 
 [ft idx] = getfeature(ds, features);
     
-clf;
+clf; hold on
 set(gcf, 'defaulttextinterpreter', 'none');
 
 for ci = 1:ds.nlabels
-    pts = ds.trainlabel == ci;
-    for i = 1:nnz(pts)
+    pts = find(ds.trainlabel == ci);
+    
+    for i = 1:length(pts)
         idx = pts(i);
         h = line(ft(idx, 1), ft(idx, 2));
         set(h, 'Marker','.', 'LineStyle', 'none', ...
@@ -17,14 +18,20 @@ for ci = 1:ds.nlabels
          'MarkerFaceColor', ds.labelcolors(ci,:), ...
          'MarkerEdgeColor', ds.labelcolors(ci,:), ...
          'MarkerSize', 8, ...
-         'ButtonDownFcn', @callback);
-    hold on
+         'ButtonDownFcn', @callback, ...
+         'Tag', num2str(idx));
+    end
 end
 
 xlabel(features{1});
 ylabel(features{2});
 title(sprintf('%s vs. %s', features{1}, features{2}));
 
-function callback(gcbo, eventdata, handles)
+function callback(src, event)
+   
+tag = str2num(get(src,'Tag'));
+viewsg(ds,tag, 99);
 
-srcdisp('hello!')
+end
+
+end
